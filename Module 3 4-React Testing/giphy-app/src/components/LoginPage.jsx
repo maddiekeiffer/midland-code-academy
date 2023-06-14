885bb485730cb44a4b7210fa31bc8f8f401d0a3f
maddiekeiffer <madisonkeiffer@gmail.com>
2023-06-13 19:43:35 -0500

    updated reqURL link

--- Module 3 4-React Testing/giphy-app/src/components/LoginPage.jsx
+++ Module 3 4-React Testing/giphy-app/src/components/LoginPage.jsx
@@ -5,43 +5,50 @@ import Input from '../styled/elements/Input';
 import axios from 'axios';
 
 
-function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const { setUser } = useUserContext();
 
-    const { setUser } = useUserContext();
+  let reqURL;
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
 
-        let reqURL = 'http://localhost:3006/' + e.target.value;
+    reqURL = 'http://localhost:3006/auth/' + e.target.value;
+    // This is because in our index.js we did app.use('/auth', authRoutes)
+    // so the url for login/register is actually this:
+    // http://localhost:3006/auth/login and http://localhost:3006/auth/register
+    // we have to use auth/login or auth/register 
+    // because we split up the files in the auth folder
 
-          try {
-            const response = await axios.post(reqURL, {
-              username: username,
-              password: password
-            });
-          
-            if (response.status === 200) {
-              setUser({ username });
-            } else {
-              console.error("Error logging in");
-            }
-          } catch (err) {
-            console.error(err);
-            if (err.response && err.response.status === 400) {
-              setError(err.response.data.error);
-            }
-          }
+      try {
+        const response = await axios.post(reqURL, {
+          username: username,
+          password: password
+        });
+  
+        if (response.status === 200) {
+          setUser({ username });
+        } else {
+          console.error("Error logging in");
+        }
+      } catch (err) {
+        console.error(err);
+        if (err.response && err.response.status === 400) {
+          setError(err.response.data.error);
+        }
+      }
     }
-    
+
     useEffect(() => {
       if (error) {
         alert(error);
         setError();
       }
-    }, [error]);
+    }, [error])
 
   return (
     <div>
